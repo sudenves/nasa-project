8a5370dfa08c51d103a69a6f19c7b8001aba621b
sudenves <evessudenur@gmail.com>
2025-10-02 00:13:10 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 import { AsteroidImpactSimulator } from './simulator/AsteroidImpactSimulator.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    
+    const splashScreen = document.getElementById('splash-screen');
+    const mainContent = document.getElementById('main-content');
+    
+    const totalAnimationDuration = 4000; 
+
     new AsteroidImpactSimulator();
+    
+    setTimeout(() => {
+        splashScreen.classList.add('hidden');
+        
+        mainContent.classList.remove('hidden');
+    }, totalAnimationDuration);
 });
